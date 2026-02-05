869da57ca49d3807229db5ce285cfb5345193947
kendryte747 <kendryte747@gmail.com>
2026-02-05 11:04:38 +0800

    (build): update helloworld makefile

--- src/applications/mkenv.mk
+++ src/applications/mkenv.mk
@@ -8,14 +8,4 @@ include $(SDK_SRC_ROOT_DIR)/.config
 
 export SDK_APPS_IMAGE_DIR := $(SDK_BUILD_IMAGES_DIR)/sdcard/app/
 
-RM = rm -rf
-ECHO = echo
-CP = cp
-MKDIR = mkdir
-SED = sed
-CAT = cat
-TOUCH = touch
-PYTHON = python3
-ZIP = zip
-
 export MKENV_INCLUDED_APPLICATIONS=1
